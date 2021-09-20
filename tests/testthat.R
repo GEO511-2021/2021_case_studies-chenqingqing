2ec9566abc0aa509bffdb38bed254a34a4f97d1b
chenqingqing <chenqingqing925@gmail.com>
2021-09-20 17:18:07 -0400

    feat: create testing for R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,15 @@
-#repo="https://raw.githubusercontent.com/AdamWilsonLabEDU/geo511_tasks_tests"
+# repo="https://raw.githubusercontent.com/AdamWilsonLabEDU/geo511_tasks_tests"
+#list basic packages needed for testing 
+list.of.packages <- c("devtools", "RCurl", "here")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
 
-questionr::qscan(list.files(pattern="*.R",recursive=T), load = TRUE, detail = FALSE)
+# repo <- "https://github.com/GEO511-2021/2021_case_studies-chenqingqing"
+# 
+# questionr::qscan(list.files(pattern="*.R",recursive=T), load = TRUE, detail = FALSE)
+# setwd(devtools::package_file())
 
-setwd(devtools::package_file())
-
-testthat::test_dir(testthat::test_path(),stop_on_failure = TRUE,reporter = "progress")
+# source(here::here("tests/testthat/test.R"))
+testthat::test_dir(testthat::test_path(), 
+                   stop_on_failure = TRUE, 
+                   reporter = "progress")
